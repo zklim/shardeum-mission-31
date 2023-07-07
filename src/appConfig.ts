16f6d12351116850f27e5747e839fd5959125e92
Zk <zklim95@hotmail.com>
2023-07-07 23:20:07 +0800

    config

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,11 +1,11 @@
 export const app: { [key: string]: string } = {
-  name: 'dApp Starter', // web app name
-  title: 'dApp Starter Boilerplate', // website title
-  description: 'dApp Starter Boilerplate by https://github.com/arisac', // description of website
+  name: 'Shardeum Mission 31', // web app name
+  title: 'Shardeum Mission 31', // website title
+  description: 'Build upgradable contract', // description of website
   keywords: 'web3, dapp, ethereum, ethers, wagmi, rainbowkit, nextjs, react, tailwindcss, sass, boilerplate', // keywords for SEO
-  url: 'https://dapp-starter.aris.ac', // website url
-  image: '/icon-512x512.png', // image url for SEO
-  favicon: '/favicon.ico', // favicon url
+  url: 'https://github.com/Shardeum/shardeum-missions/tree/main/missions/new/31', // website url
+  image: 'https://icodrops.com/wp-content/uploads/2022/10/H88Y46ND_400x400.jpg', // image url for SEO
+  favicon: 'https://icodrops.com/wp-content/uploads/2022/10/H88Y46ND_400x400.jpg', // favicon url
   twitter: '@arisdotac', // twitter account
   themeColor: '#FFFFFF', // theme color
   themeColorDark: '#000000', // theme color dark
